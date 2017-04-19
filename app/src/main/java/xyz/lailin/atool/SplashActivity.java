eaff9ff07c1616ee660e49dc92fd4ada5c6ed17e
e <1@lailin.xyz>
2017-04-19 15:13:23 +0800

    update

---
+++ app/src/main/java/xyz/lailin/atool/SplashActivity.java
@@ -0,0 +1,26 @@
+package xyz.lailin.atool;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 启动页
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                /* Create an Intent that will start the Main WordPress Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 1500);
+    }
+}
