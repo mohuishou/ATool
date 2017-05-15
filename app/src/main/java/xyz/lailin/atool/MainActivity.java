b74ea7afcd8a3fef5ea62b175c9f0ebf488e58b3
mohuishou <1@lailin.xyz>
2017-05-15 22:42:15 +0800

    update

--- app/src/main/java/xyz/lailin/atool/MainActivity.java
+++ app/src/main/java/xyz/lailin/atool/MainActivity.java
@@ -196,7 +196,6 @@ public class MainActivity extends AppCompatActivity {
                                 comfort=Math.abs(prevData[i]-event.values[i]);
                             }
                         }
-                        Log.i("test:",(-comfort+5f)+"");
                         setComfortRate(rate);
                     }
 
